0f861bffe806921b77c256406a1b30ac8dd416b7
Kyle Bondo <gagglepod@gmail.om>
2022-06-18 23:35:06 -0500

    Firebase config and composables ready

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyByKbViZ1QTADUriMUWrmJx1gllDFo4pn4",
+  authDomain: "uproar-playlist.firebaseapp.com",
+  projectId: "uproar-playlist",
+  storageBucket: "uproar-playlist.appspot.com",
+  messagingSenderId: "748309353895",
+  appId: "1:748309353895:web:487ab0ebc5e376b0091282",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebsae.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
