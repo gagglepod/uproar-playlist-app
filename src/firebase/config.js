85f5c98ba1cb7348ed26b10ac4dd7b55b36730d6
Kyle Bondo <gagglepod@gmail.om>
2022-06-25 10:11:47 -0500

    Added in isPending boolean for composable await

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -19,6 +19,6 @@ const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
 
 // timestamp
-const timestamp = firebsae.firestore.FieldValue.serverTimestamp;
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
 export { projectFirestore, projectAuth, timestamp };
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Login from "../views/auth/Login.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
 ];
 
